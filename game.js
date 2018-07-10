c550fe44fe20e88809757e51783ad0e328d349ce
Paula Balcells <paulabalcells@gmail.com>
2018-07-10 08:50:29 +0200

    Detecta una colisión por obstáculo. Propiedad collision añadida a Obstacle

--- game.js
+++ game.js
@@ -25,13 +25,13 @@ Game.prototype._drawObstacle = function () {
 
 Game.prototype.start = function () {
     this.ctx.canvas = document.getElementById('canvas');
-    this.ctx.canvas.addEventListener('click', this.on_canvas_click.bind(this), false);
+    this.ctx.canvas.addEventListener('click', this.onCanvasClick.bind(this), false);
     this.intervalGame = window.requestAnimationFrame(this._update.bind(this));
     this._generateObstacle();
     this.intervalObstacle = setInterval(this._generateObstacle.bind(this), 1500);
 }
 
-Game.prototype.on_canvas_click = function (e) {
+Game.prototype.onCanvasClick = function (e) {
     var x = e.clientX - this.ctx.canvas.offsetLeft;
     var y = e.clientY - this.ctx.canvas.offsetTop;
 
@@ -44,8 +44,6 @@ Game.prototype.on_canvas_click = function (e) {
     this.fish.start();
 }
 
-Game.prototype.assignControlToTouchEvents = function () {}
-
 Game.prototype._generateObstacle = function () {
     this.obstacles.push(new Obstacle());
     console.log(this.obstacles);
@@ -54,9 +52,29 @@ Game.prototype._generateObstacle = function () {
 Game.prototype._checkObstacle = function () {
     this.obstacles.forEach(function(obstacle, index){
         if (obstacle.y < 0){
+            obstacle.clear();
             this.obstacles.splice(index,1);
         }
     }.bind(this));
+}
+
+Game.prototype._collision = function () {
+    this.obstacles.forEach(function(obstacle){
+        var cornerTopLeft = [obstacle.x, obstacle.y];
+        var cornerTopRight = [obstacle.x+20, obstacle.y];
+        var cornerBottomLeft = [obstacle.x, obstacle.y+20];
+        var cornerBottomRight = [obstacle.x+20, obstacle.y+20];
+
+        var corners = [cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight];
+        corners.forEach(function(corner){
+            if (corner[0]  >=this.fish.x && corner[0] <= this.fish.x+20 && corner[1] >= this.fish.y && corner[1] <= this.fish.y+20){
+                if (obstacle.collision === false){
+                    console.log('collision');
+                    obstacle.collision = true;
+                } 
+            }
+        }.bind(this));
+    }.bind(this))
 }
 
 Game.prototype._update = function () {
@@ -64,6 +82,7 @@ Game.prototype._update = function () {
     this._drawFish();
     this._drawObstacle();
     this._checkObstacle();
+    this._collision();
     this.intervalGame = window.requestAnimationFrame(this._update.bind(this));
 }
 
--- obstacle.js
+++ obstacle.js
@@ -2,11 +2,18 @@ function Obstacle(){
     this.x = Math.floor(Math.random()*361)+20;
     this.y = 630;
     this.intervalId = undefined;
+    this.collision = false;
 }
 
 Obstacle.prototype.start = function () {
     if (!this.intervalId) {
         this.intervalId = setInterval(this.move.bind(this), 50);
+    }
+}
+
+Obstacle.prototype.clear = function () {
+    if (this.intervalId) {
+        clearInterval(this.intervalId);
     }
 }
 
